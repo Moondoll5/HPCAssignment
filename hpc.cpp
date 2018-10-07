fb8d614e05706b330f26d06304df3786fbc12441
Imaan Dolley <pink-angel-911@hotmail.com>
2018-10-07 18:47:00 +0200

    Can read file's lines into an array

--- hpc.cpp
+++ hpc.cpp
@@ -4,19 +4,31 @@
 #include <string>
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+	cout << argv[0] << endl;//File path
+	cout << argv[1] << endl;//-i
+	cout <<argv[2] << endl;//inputfile
+
+	//Name of the input file given as argument
+	string filename = argv[2];
 
 	//Reading a file
 	ifstream infile;
-	cout << "Enter filename:" << endl;
-	string filename;
-	cin >> filename;
 	infile.open(filename);
-	string data;
-	infile >> data;
 
-	//Write data to screen
-	cout << data << endl;
+	//Initialise variables
+	string input[4]={};
+	int count = 0;
+	string line;
+
+	//Read file into array
+	while(getline(infile, line)) {
+        input[count] = line;
+        count++;
+    }
+
+    //Create A and B subsets
+    
 
 	//Close the file
 	infile.close();
